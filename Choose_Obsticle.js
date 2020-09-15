28c1acdcf22544a6ed0b7c25ff1882ec9637d7e5
Error Message <cormacflanagan12@gmail.com>
2020-09-15 11:25:25 +1000

    Working Prototype of Game DinoJump Now With: Fancy Graphics Fancy Scorecard Updated Hitbox detection Removed Old Code Added New Animation system for cactus and allowed for more customisable release settings, Fixed position of cactus to floor
    Add Variable Enemys, Current Movefunctionality us broken

--- Choose_Obsticle.js
+++ Choose_Obsticle.js
@@ -1,28 +1,55 @@
-var release = setInterval(releaseMove, 5000)
 var running = false;
-
-
+var cacCount = 0;
 function myMove() {
-    running = true;
-    var elem = document.getElementById("regCactus");
+    cacCount ++
+    var elem = document.getElementById(`enemy`);
+    randomType(elem, 0, 2);
     var pos = 600;
     var id = setInterval(frame,5);
     function frame() {
+        if (pos <= 10) {
+            elem.style.visibility = 'hidden';
+        } else {
+            elem.style.visibility = 'visible';
+        }
         if (pos <= 5) {
             clearInterval(id);
-            document.getElementById("regCactus").style.visibility = 'hidden';
             running = false;
+            cacCount = 0;
         } else {
             pos -= 2;
-            elem.style.left = pos + "px";
-            document.getElementById("Debug").innerHTML = running;
         }
+        elem.style.left = pos + "px";
     }
 }
 
 function releaseMove() {
     if (running === false) {
-        document.getElementById("regCactus").style.visibility = 'visible';
-        myMove();
+        running = true;
+        setTimeout(function() {
+            var release = myMove();
+        }, 10*Math.floor((Math.random() * 100) + 10));
     }
 }
+
+function randomType(changed, enMintype, enMaxtype) {
+    var Enemy_Type = Math.floor(Math.random(enMintype) * Math.floor(enMaxtype));
+    var x, y;
+    switch (Enemy_Type) {
+        case 0:
+            x = "regCactus";
+            y = ["pterodactyl"]
+            break
+        case 1:
+            x = "pterodactyl"
+            y = ["regCactus"]
+            break
+    }
+    for (i of y) {
+        changed.classList.remove(i)
+    }
+    changed.classList.add(x)
+    document.getElementById("Debug").innerHTML(Enemy_Type);
+    return x;
+}
+
